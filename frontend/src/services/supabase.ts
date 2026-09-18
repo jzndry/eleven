c5b233f64a63d25c0e4c828a9f7e49505f285edf
jan <jandry.rodriguez@outlook.com>
2026-09-18 23:33:24 +0100

    chore: update supabase secret anon key naming for easier understanding with new supabase system

--- frontend/src/services/supabase.ts
+++ frontend/src/services/supabase.ts
@@ -1,6 +1,6 @@
 import 'react-native-get-random-values';
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY} from '@env';
+import { SUPABASE_URL, SUPABASE_ANON_KEY} from '@env';
 
 // Initialise the Supabase client using environment variables
-export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
