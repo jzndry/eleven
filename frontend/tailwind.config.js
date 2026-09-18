78ae1cf035ee573ddce29fdbd18c9af50ecbc86e
jan <jandry.rodriguez@outlook.com>
2026-09-18 23:34:18 +0100

    build: update content exports to appropriarelty match new folder restructure

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: This has to be manually kept in sync with the content array in babel.config.js for nativewind/babel
-  content: ["./app/**/*.{js,jsx,ts,tsx}",
-           "./components/**/*.{js,jsx,ts,tsx}",
-      "./app/(tabs)/**/*.{js,jsx,ts,tsx}",
-       "./app/(auth)/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
